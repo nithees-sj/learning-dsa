11bb4cc9eec9a78213407c71d0089a016608ecef
nithees-sj <nitheessj@gmail.com>
2026-06-04 23:19:25 +0530

    twosum sum using two pointer approach

---
+++ Two Pointer approach/TwoSum.java
@@ -0,0 +1,24 @@
+public class TwoSum{
+    public static void main(String[] args) {
+        int arr[] = {2,7,11,15};
+        int target = 9;
+
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left<right){
+            int sum = arr[left]+arr[right];
+
+            if(sum == target){
+                System.out.println("Target Found");
+                break;
+            }
+            else if(sum<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+    }
+}
