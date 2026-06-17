a3b4eab91d7984f3670fbdd6259ba98634912a04
nithees-sj <nitheessj@gmail.com>
2026-06-17 16:03:37 +0530

    Highest occuring element

---
+++ Arrays Foundation/HighestOccuring.java
@@ -0,0 +1,25 @@
+
+import java.util.HashMap;
+
+public class HighestOccuring{
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,3,4,4,4,4};
+
+        //To find this need to find their frequemcy and check soo on and return the maximum frequency element 
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int maxfreq=0;
+        int maxe=0;
+        //Finding frequency
+        for(int num : arr){
+            int freq = map.getOrDefault(num, 0)+1;
+            map.put(num,freq);
+
+            if(freq>maxfreq){
+                maxfreq = freq;
+                maxe=num;
+            }
+        }
+        System.out.println(maxe);
+    }
+}
