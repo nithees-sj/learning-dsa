0918e2bff9510e8a795bbd015ac1b9eb735b3030
nithees-sj <nitheessj@gmail.com>
2026-06-17 21:36:47 +0530

    reverse arrays

---
+++ Arrays Foundation/ReverseArray.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+public class ReverseArray{
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
+
+        int start=0;
+        int end = arr.length-1;
+
+        while(start<end){
+            int temp=arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
