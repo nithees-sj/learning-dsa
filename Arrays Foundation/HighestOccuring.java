29b6ffc355f15377238e851d25073beb6f73c290
nithees-sj <nitheessj@gmail.com>
2026-06-18 21:26:54 +0530

    higher frequency code

--- Arrays Foundation/HighestOccuring.java
+++ Arrays Foundation/HighestOccuring.java
@@ -3,23 +3,22 @@ import java.util.HashMap;
 
 public class HighestOccuring{
     public static void main(String[] args) {
-        int arr[] = {1,1,2,3,4,4,4,4};
+        int arr[] = {1,1,2,3,4,4,4};
 
         //To find this need to find their frequemcy and check soo on and return the maximum frequency element 
 
         HashMap<Integer,Integer> map = new HashMap<>();
-        int maxfreq=0;
-        int maxe=0;
-        //Finding frequency
+        int maxf = 0;
+        int maxe = 0;
         for(int num : arr){
-            int freq = map.getOrDefault(num, 0)+1;
+            int freq = (map.getOrDefault(num, 0)+1);
             map.put(num,freq);
 
-            if(freq>maxfreq){
-                maxfreq = freq;
-                maxe=num;
+            if(freq>maxf){
+                maxf = freq;
+                maxe = num;
             }
         }
-        System.out.println(maxe);
+        System.out.println(maxe + " " + maxf);
     }
 }
