b6d844d72a76fcf4d87b2656c61a4a76e220a2b9
nithees-sj <nitheessj@gmail.com>
2026-08-17 22:21:59 +0530

    Changed the code

--- Striver Tuf/SortColors.java
+++ Striver Tuf/SortColors.java
@@ -1,33 +1,42 @@
 import java.util.*;
-public class SortColors{
-    public static void main(String[] args) {
-        int nums[] = {2,0,2,1,1,0};
 
-        //Threee pointer approach 
-        //This approach works like the swapping mechanism
+public class SortColors {
+    public static void main(String[] args) {
+        int nums[] = {2, 0, 2, 1, 1, 0};
+
+        // Three pointer approach
+        // low  -> boundary for 0
+        // mid  -> current element
+        // high -> boundary for 2
 
         int low = 0;
         int mid = 0;
-        int high = nums.length-1;
+        int high = nums.length - 1;
 
-        while(mid<=high){
-            if(nums[mid] == 1){
+        while (mid <= high) {
+
+            if (nums[mid] == 1) {
                 mid++;
             }
-            else if(nums[mid] == 2){
+
+            else if (nums[mid] == 2) {
                 int temp = nums[mid];
                 nums[mid] = nums[high];
                 nums[high] = temp;
+
                 high--;
             }
-            else{
+
+            else { // nums[mid] == 0
                 int temp = nums[low];
                 nums[low] = nums[mid];
                 nums[mid] = temp;
+
                 low++;
                 mid++;
             }
         }
+
         System.out.println(Arrays.toString(nums));
     }
 }
