a2b50268f8e36a32dc293d1913a0717e90a9fffc
nithees-sj <nitheessj@gmail.com>
2026-08-18 21:33:28 +0530

    maximum sum of sub array

---
+++ Striver Tuf/maxSubarray.java
@@ -0,0 +1,14 @@
+public class maxSubarray{
+    public static void main(String[] args) {
+        int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 0; i < nums.length-1; i++) {
+            currentSum = Math.max(nums[i], currentSum+nums[i]);
+            maxSum = Math.max(currentSum,maxSum);
+        }
+        System.out.println(maxSum);
+    }
+}
